7b055e989f5f2e9b456cab349a3fde57f3342f91
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2024-02-15 15:22:13 +1100

    Reference Js in html file & Create wrapper div

---
+++ etch-a-sketch.js
@@ -0,0 +1,6 @@
+// create div container
+const contain = document.querySelector('body');
+
+const container = document.createElement('div');
+container.setAttribute('id', 'wrapper');
+contain.appendChild(container);
