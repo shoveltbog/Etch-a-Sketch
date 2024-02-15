4fd665f8fcec82b38dc3af4aa6396f8bcfc906d5
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2024-02-15 17:18:42 +1100

    Add flexbox to 16 x 16 div grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,14 @@
 const container = document.querySelector('.container');
 
 // create 16 x 16 div grid
-for (var i = 0; i < 16; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid');
-    container.appendChild(gridDiv);
+for (let i = 0; i < 16; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    container.appendChild(rowDiv);
+
+    for (let j = 0; j < 16; j++) {
+        const colDiv = document.createElement('div');
+        colDiv.classList.add('column');
+        container.appendChild(colDiv);
+    }
 }
