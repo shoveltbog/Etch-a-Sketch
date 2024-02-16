3912bbbd13c9f196ec92d39b6f1cd05236128837
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2024-02-16 14:57:32 +1100

    change 16 x16 grid into function for reusability

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,19 +1,21 @@
 const container = document.querySelector('.container');
 
-// create 16 x 16 div grid
-for (let i = 0; i < 16; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row', 'cell');
-    container.appendChild(rowDiv);
+// create div grid function
+function createGrid(container, rows, columns) {
+    for (let i = 0; i < rows; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row', 'cell');
+        container.appendChild(rowDiv);
 
-    for (let j = 0; j < 16; j++) {
-        const colDiv = document.createElement('div');
-        colDiv.classList.add('column', 'cell');
-        rowDiv.appendChild(colDiv);
+        for (let j = 0; j < columns; j++) {
+            const colDiv = document.createElement('div');
+            colDiv.classList.add('column', 'cell');
+            rowDiv.appendChild(colDiv);
 
-        colDiv.addEventListener('mouseenter', function() {
-            this.classList.add('hovered');
-        });
+            colDiv.addEventListener('mouseenter', function() {
+                this.classList.add('hovered');
+            });
+        }
     }
 }
 
@@ -22,8 +24,15 @@ for (let i = 0; i < 16; i++) {
 const btn = document.querySelector('body');
 
 const gridButton = document.createElement('button');
-gridButton.innerText = "Input grid size";
+gridButton.innerText = "Input Grid Size";
 btn.insertBefore(gridButton, container);
+
+// Function to alert user to input grid size upon click
+gridButton.addEventListener('click', () => {
+    const userInput = prompt("Enter the size of the grid (e.g., 16 for a 16x16 grid):");
+    // use number from prompt to make grid size
+
+});
 
 
 
