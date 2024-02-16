6342373ee15709d4ff2da8f7ea8e954bd2687625
shoveltbog <132999441+shoveltbog@users.noreply.github.com>
2024-02-16 13:13:21 +1100

    Add hover effect to change cells colour

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,12 +3,16 @@ const container = document.querySelector('.container');
 // create 16 x 16 div grid
 for (let i = 0; i < 16; i++) {
     const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
+    rowDiv.classList.add('row', 'cell');
     container.appendChild(rowDiv);
 
     for (let j = 0; j < 16; j++) {
         const colDiv = document.createElement('div');
-        colDiv.classList.add('column');
-        container.appendChild(colDiv);
+        colDiv.classList.add('column', 'cell');
+        rowDiv.appendChild(colDiv);
+
+        colDiv.addEventListener('mouseenter', function() {
+            this.classList.add('hovered');
+        });
     }
 }
